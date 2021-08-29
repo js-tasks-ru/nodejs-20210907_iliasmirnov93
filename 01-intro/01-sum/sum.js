860f79a28b1763235dd4340be88f4762345a635f
Ilya Smirnov <iliasmirnov93@mail.ru>
2021-08-29 21:51:36 +0300

    1st task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,12 @@
 function sum(a, b) {
   /* ваш код */
+  if ([a, b].some((value) => typeof value !== 'number')) {
+    throw new TypeError();
+  }
+
+  return a + b;
+
 }
+
 
 module.exports = sum;
