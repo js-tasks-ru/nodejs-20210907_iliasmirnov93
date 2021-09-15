22a974921f1ac1af82fe5ac617fa555f8e790d0c
Ilya Smirnov <iliasmirnov93@mail.ru>
2021-09-15 17:20:56 +0300

    5 task

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,9 +4,15 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.encoding = options.encoding;
   }
 
   _transform(chunk, encoding, callback) {
+    
+   
+      callback(LimitExceededError, );
+   
 
   }
 }
