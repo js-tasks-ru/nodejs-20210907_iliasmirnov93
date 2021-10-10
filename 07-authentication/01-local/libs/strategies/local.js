e782d55582ea58330cc1531b35bf7fccd3d35bb7
Ilya Smirnov <iliasmirnov93@mail.ru>
2021-10-10 23:15:11 +0300

    auth 1 task

--- 07-authentication/01-local/libs/strategies/local.js
+++ 07-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,24 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
+const mongoose = require('mongoose');
 
 module.exports = new LocalStrategy(
-    {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
-    },
+  { usernameField: 'email', session: false },
+  async function (email, password, done) {
+
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return done(null, false, 'Нет такого пользователя');
+      }
+      const passwordHashGeneration = await user.checkPassword(password);
+      if (!passwordHashGeneration) {
+        return done(null, false, 'Неверный пароль');
+      }
+      return done(null, user);
+    }
+    catch (err) {
+      done(err);
+    }
+  },
 );
